c5fe34bb59a1c5cec5b536bfd44b421d0db90d07
alazar-des <alazar.des@gmail.com>
2021-12-09 12:03:20 +0300

    trigger to decrease quantity after order

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+-- Create after insert trigger
+-- a trigger that decreases the quantity of an item after adding a new order
+DELIMITER //
+CREATE TRIGGER quantity_after_order
+AFTER INSERT
+ON orders FOR EACH ROW
+BEGIN
+	UPDATE items
+	SET quantity = quantity - NEW.number
+	WHERE NEW.item_name = items.name;
+END; //
+DELIMITER ;
