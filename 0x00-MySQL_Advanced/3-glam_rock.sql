dc050d371aba6e7829ff1e2479a893a38f47550e
alazar-des <alazar.des@gmail.com>
2021-12-09 11:28:05 +0300

    when null use 2020 fix

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- list all bands with Glam rock as their main style, ranked by longevity
 -- select where syle = "Glam rock"
-SELECT band_name, IFNULL(split, YEAR(CURDATE())) - formed AS lifespan
+SELECT band_name, IFNULL(split, 2020) - formed AS lifespan
        FROM metal_bands
        WHERE style LIKE '%Glam rock%'
-       ORDER BY IFNULL(split, YEAR(CURDATE())) - formed DESC;
+       ORDER BY IFNULL(split, 2020) - formed DESC;
