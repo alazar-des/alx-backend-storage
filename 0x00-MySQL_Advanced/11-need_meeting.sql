b0c4ba50c08085e72f530f98d7d4c18eb7bb8ae4
alazar-des <alazar.des@gmail.com>
2021-12-09 17:17:16 +0300

    view

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,7 @@
+-- view
+-- create view
+CREATE VIEW need_meeting AS
+       SELECT name
+       FROM students
+       WHERE score < 80 AND
+       (last_meeting IS NULL OR DATEDIFF(CURDATE(), last_meeting) > 30);
